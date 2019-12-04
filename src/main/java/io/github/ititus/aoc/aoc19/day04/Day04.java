3f8d16081cb2dc312bc07a05f60c63263c8bc5b3
iTitus <iTitus@users.noreply.github.com>
2019-12-04 10:34:04 +0100

    Day 04

---
+++ src/main/java/io/github/ititus/aoc/aoc19/day04/Day04.java
@@ -0,0 +1,63 @@
+package io.github.ititus.aoc.aoc19.day04;
+
+import io.github.ititus.aoc.InputProvider;
+
+import java.util.Arrays;
+
+public class Day04 {
+
+    public static void main(String[] args) {
+        int[] range = Arrays.stream(InputProvider.readString(2019, 4).split("-")).map(String::strip).mapToInt(Integer::parseInt).toArray();
+        int lower = range[0];
+        int higher = range[1];
+
+
+        int count1 = 0, count2 = 0;
+        for (int i = lower; i <= higher; i++) {
+            int minRepeatingCount = getMinRepeatingCount(i);
+            if (minRepeatingCount >= 2) {
+                count1++;
+                if (minRepeatingCount == 2) {
+                    count2++;
+                }
+            }
+        }
+
+        // 1
+        System.out.println(count1);
+
+        // 2
+        System.out.println(count2);
+    }
+
+    private static int getMinRepeatingCount(int password) {
+        char[] chars = String.valueOf(password).toCharArray();
+        if (chars.length != 6) {
+            return -1;
+        }
+
+        int minRepeatingCount = -1;
+        int curRepeatingCount = 1;
+        char last = chars[0];
+        for (int i = 1; i < chars.length; i++) {
+            char c = chars[i];
+
+            if (c < last) {
+                return -1;
+            } else if (c == last) {
+                curRepeatingCount++;
+            }
+
+            if (c > last || i == chars.length - 1) {
+                if (curRepeatingCount > 1 && (minRepeatingCount == -1 || curRepeatingCount < minRepeatingCount)) {
+                    minRepeatingCount = curRepeatingCount;
+                }
+                curRepeatingCount = 1;
+            }
+
+            last = c;
+        }
+
+        return minRepeatingCount;
+    }
+}
