1542e94d0d37924fd9b9afbed54528674ad4c1f4
iTitus <iTitus@users.noreply.github.com>
2019-12-06 11:17:50 +0100

    Day 06

---
+++ src/main/java/io/github/ititus/aoc/day06/Day06.java
@@ -0,0 +1,69 @@
+package io.github.ititus.aoc.day06;
+
+import io.github.ititus.aoc.InputProvider;
+import io.github.ititus.math.graph.Edge;
+import io.github.ititus.math.graph.Graph;
+import io.github.ititus.math.graph.Vertex;
+import io.github.ititus.math.graph.algorithm.Dijkstra;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class Day06 {
+
+    public static void main(String[] args) {
+        Graph<String> g = new Graph<>();
+        InputProvider.lines(2019, 6)
+                .map(s -> s.split("\\)"))
+                .forEach(s -> {
+                    Vertex<String> vs = g.getVertex(s[1]).orElseGet(() -> g.addVertex(s[1]));
+                    Vertex<String> ve = g.getVertex(s[0]).orElseGet(() -> g.addVertex(s[0]));
+                    g.addEdge(vs, ve);
+                });
+
+        // 1
+        System.out.println(
+                g.getVertices().stream()
+                        .mapToInt(v -> computeOrbitCount(g, v))
+                        .sum()
+        );
+
+        // 2
+        Vertex<String> youOrbiting = getOrbiting(g, g.getVertex("YOU").orElseThrow()).orElseThrow();
+        Vertex<String> santaOrbiting = getOrbiting(g, g.getVertex("SAN").orElseThrow()).orElseThrow();
+        Dijkstra<String>.Result r = new Dijkstra<>(g, youOrbiting, false).findShortestPaths();
+        System.out.println(r.getShortestPathLength(santaOrbiting));
+    }
+
+    private static Map<Vertex<String>, Integer> orbitingCount = new HashMap<>();
+
+    private static int computeOrbitCount(Graph<String> g, Vertex<String> v) {
+        Integer cached = orbitingCount.get(v);
+        if (cached != null) {
+            return cached;
+        }
+
+        Optional<Vertex<String>> orbiting = getOrbiting(g, v);
+        if (orbiting.isEmpty()) {
+            orbitingCount.put(v, 0);
+            return 0;
+        }
+
+        int count = 1 + computeOrbitCount(g, orbiting.get());
+        orbitingCount.put(v, count);
+        return count;
+    }
+
+    private static Optional<Vertex<String>> getOrbiting(Graph<String> g, Vertex<String> v) {
+        Set<Edge<String>> outgoingSet = g.getOutgoingEdges(v);
+        if (outgoingSet.size() > 1) {
+            throw new RuntimeException();
+        } else if (outgoingSet.isEmpty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(outgoingSet.iterator().next().getEnd());
+    }
+}
