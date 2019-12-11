d1ff0b92004765db8670bd160a570c91e80d85e9
iTitus <iTitus@users.noreply.github.com>
2019-12-11 10:12:16 +0100

    Move some classes to lib

--- src/main/java/io/github/ititus/aoc/aoc19/day03/Day03.java
+++ src/main/java/io/github/ititus/aoc/aoc19/day03/Day03.java
@@ -1,15 +1,15 @@
 package io.github.ititus.aoc.aoc19.day03;
 
 import io.github.ititus.aoc.InputProvider;
-import io.github.ititus.aoc.aoc19.Vec2i;
+import io.github.ititus.math.vector.Vec2i;
 
 import java.util.*;
 
 public class Day03 {
 
     public static void main(String[] args) {
-        doExercises(List.of("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"));
-        doExercises(List.of("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"));
+        // doExercises(List.of("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"));
+        // doExercises(List.of("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"));
         doExercises(InputProvider.readAllLines(2019, 3));
     }
 
@@ -44,7 +44,7 @@ public class Day03 {
         Vec2i pos = new Vec2i();
         int steps = 0;
         for (String cmd : commands) {
-            List<Vec2i> newPos = pos.move(cmd);
+            List<Vec2i> newPos = move(pos, cmd);
             for (Vec2i p : newPos) {
                 wire2Pos.putIfAbsent(p, ++steps);
             }
@@ -58,5 +58,37 @@ public class Day03 {
         intersect.keySet().retainAll(b.keySet());
         b.forEach((posB, stepsB) -> intersect.computeIfPresent(posB, (posA, stepsA) -> stepsA + stepsB));
         return intersect;
+    }
+
+    /**
+     * Moves the starting position by the given command.
+     *
+     * @param pos     starting position
+     * @param command "&lt;direction&gt;&lt;number&gt;"
+     * @return path from this position to the target position
+     */
+    private static List<Vec2i> move(Vec2i pos, String command) {
+        char c = command.charAt(0);
+        int dirX = 0, dirY = 0;
+
+        if (c == 'R') {
+            dirX = 1;
+        } else if (c == 'L') {
+            dirX = -1;
+        } else if (c == 'U') {
+            dirY = 1;
+        } else if (c == 'D') {
+            dirY = -1;
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+        List<Vec2i> path = new ArrayList<>();
+        int amount = Integer.parseInt(command.substring(1));
+        for (int i = 1; i <= amount; i++) {
+            path.add(new Vec2i(pos.getX() + i * dirX, pos.getY() + i * dirY));
+        }
+
+        return path;
     }
 }
