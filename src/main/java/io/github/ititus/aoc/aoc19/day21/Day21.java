a5a613fc549a6a9cd2db2c4c8bac9f18825b02e8
iTitus <iTitus@users.noreply.github.com>
2019-12-21 20:04:09 +0100

    Day 21

---
+++ src/main/java/io/github/ititus/aoc/aoc19/day21/Day21.java
@@ -0,0 +1,46 @@
+package io.github.ititus.aoc.aoc19.day21;
+
+import io.github.ititus.aoc.InputProvider;
+import io.github.ititus.math.number.BigIntegerMath;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class Day21 {
+
+    public static void main(String[] args) {
+        String input = InputProvider.readString(2019, 21);
+        BigInteger[] memory = Arrays.stream(input.split(",")).map(String::strip).map(BigIntegerMath::of).toArray(BigInteger[]::new);
+
+        SpringDroid sd = new SpringDroid(memory);
+
+        // 1
+        System.out.println("### 1 ###");
+        int r1 = sd.runSpringScript(
+                "NOT J J",
+                "AND A J",
+                "AND B J",
+                "AND C J",
+                "NOT J J",
+                "AND D J",
+                "WALK"
+        );
+        System.out.println(r1);
+
+        // 2
+        System.out.println("### 2 ###");
+        int r2 = sd.runSpringScript(
+                "NOT J J",
+                "AND A J",
+                "AND B J",
+                "AND C J",
+                "NOT J J",
+                "AND D J",
+                "OR E T",
+                "OR H T",
+                "AND T J",
+                "RUN"
+        );
+        System.out.println(r2);
+    }
+}
