ecf024ae82376720a0e34099afef0efc95132ced
iTitus <iTitus@users.noreply.github.com>
2020-12-03 11:28:25 +0100

    Day 03

---
+++ src/main/java/io/github/ititus/aoc/aoc20/day03/Day03.java
@@ -0,0 +1,49 @@
+package io.github.ititus.aoc.aoc20.day03;
+
+import io.github.ititus.aoc.common.Aoc;
+import io.github.ititus.aoc.common.AocInput;
+import io.github.ititus.aoc.common.AocSolution;
+import io.github.ititus.math.vector.Vec2i;
+
+import java.util.Arrays;
+
+@Aoc(year = 2020, day = 3)
+public class Day03 implements AocSolution {
+
+    private TreeMap map;
+
+    @Override
+    public void executeTests() {
+    }
+
+    @Override
+    public void readInput(AocInput input) {
+        map = new TreeMap(input.readAllLines());
+        if (map.hasTree(0, 0)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public Object part1() {
+        Vec2i slope = new Vec2i(3, 1);
+        return map.countTreesEncountered(slope);
+    }
+
+    @Override
+    public Object part2() {
+        Vec2i[] slopes = {
+                new Vec2i(1, 1),
+                new Vec2i(3, 1),
+                new Vec2i(5, 1),
+                new Vec2i(7, 1),
+                new Vec2i(1, 2)
+        };
+
+        return Arrays
+                .stream(slopes)
+                .mapToLong(map::countTreesEncountered)
+                .reduce((i, j) -> i * j)
+                .orElseThrow();
+    }
+}
---
+++ src/main/java/io/github/ititus/aoc/aoc20/day03/TreeMap.java
@@ -0,0 +1,66 @@
+package io.github.ititus.aoc.aoc20.day03;
+
+import io.github.ititus.math.vector.Vec2i;
+
+import java.util.BitSet;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TreeMap {
+
+    private final int width, height;
+    private final BitSet trees;
+
+    public TreeMap(List<String> input) {
+        this.height = input.size();
+        this.width = input.get(0).length();
+
+        this.trees = new BitSet(width * height);
+
+        for (int y = 0; y < height; y++) {
+            String line = input.get(y);
+            for (int x = 0; x < width; x++) {
+                char tree = line.charAt(x);
+                if (tree == '#') {
+                    setTree(x, y);
+                } else if (tree != '.') {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
+    }
+
+    private void setTree(int x, int y) {
+        if (x < 0 || y < 0 || x >= width || y >= height) {
+            throw new IndexOutOfBoundsException();
+        }
+        trees.set(y * width + x);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public boolean hasTree(Vec2i pos) {
+        return hasTree(pos.getX(), pos.getY());
+    }
+
+    public boolean hasTree(int x, int y) {
+        if (x < 0 || y < 0 || y >= height) {
+            throw new IndexOutOfBoundsException();
+        }
+        x %= width;
+        return trees.get(y * width + x);
+    }
+
+    public long countTreesEncountered(Vec2i slope) {
+        return Stream
+                .iterate(new Vec2i(), pos -> pos.getY() < height, slope::add)
+                .filter(this::hasTree)
+                .count();
+    }
+}
