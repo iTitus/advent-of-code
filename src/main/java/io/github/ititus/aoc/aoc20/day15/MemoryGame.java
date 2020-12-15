b0db4099a63b500f66aee9c66c78c43c2de7f939
iTitus <iTitus@users.noreply.github.com>
2020-12-15 11:04:41 +0100

    optimize

--- src/main/java/io/github/ititus/aoc/aoc20/day15/MemoryGame.java
+++ src/main/java/io/github/ititus/aoc/aoc20/day15/MemoryGame.java
@@ -1,12 +1,12 @@
 package io.github.ititus.aoc.aoc20.day15;
 
-import it.unimi.dsi.fastutil.ints.*;
-
-import java.util.function.IntConsumer;
+import it.unimi.dsi.fastutil.ints.Int2IntMap;
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
+import it.unimi.dsi.fastutil.ints.IntList;
 
 public class MemoryGame {
 
-    private final Int2ObjectMap<IntIntPair> numbers;
+    private final Int2IntMap numbers;
     private int turn;
     private int lastNumber;
 
@@ -15,33 +15,33 @@ public class MemoryGame {
             throw new IllegalArgumentException();
         }
 
-        this.numbers = new Int2ObjectOpenHashMap<>();
+        this.numbers = new Int2IntOpenHashMap();
         this.turn = 1;
-        numbers.forEach((IntConsumer) this::say);
+        numbers.forEach((int n) -> {
+            this.numbers.put(n, turn++);
+            this.lastNumber = n;
+        });
     }
 
     public int run(int turns) {
+        if (turn - 1 > turns) {
+            throw new RuntimeException();
+        }
+
         while (turn <= turns) {
             nextTurn();
         }
+
         return lastNumber;
     }
 
     public void nextTurn() {
-        IntIntPair lastTurns = numbers.get(lastNumber);
-        if (lastTurns.leftInt() == 0) {
-            say(0);
+        int lastTurn = turn++ - 1;
+        int lastSeen = numbers.put(lastNumber, lastTurn);
+        if (lastSeen == 0) {
+            lastNumber = 0;
         } else {
-            say(lastTurns.rightInt() - lastTurns.leftInt());
+            lastNumber = lastTurn - lastSeen;
         }
-    }
-
-    private void say(int n) {
-        IntIntPair lastTurns = numbers.computeIfAbsent(n, k -> IntIntMutablePair.of(0, 0));
-        lastTurns.left(lastTurns.rightInt());
-        lastTurns.right(turn);
-
-        lastNumber = n;
-        turn++;
     }
 }
